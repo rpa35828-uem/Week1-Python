d69832a453c06030e4ee9db1fb2abb04fff5c9a3
rpa35828-uem <rpa35828@gmail.com>
2025-07-28 12:06:13 +0530

    added by me python file

---
+++ week1 ass2.py
@@ -0,0 +1,20 @@
+r07
+nuihpkjhuiuye
+lkmkioijkiewh78hewuiyha780y6wear
+
+
+
+ytud5{
+
+    kopkop9poirdg
+
+    rtd
+}
+
+,mkl;jokre
+
+l,mipkopkrelp[,mpore
+              
+              ';p[op[re.KeyboardInterrupt' \
+              '
+              ][pp-ololer[ki]]]']
